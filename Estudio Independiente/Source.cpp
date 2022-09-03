6ec7b5b02448da99fb4181bb1309a8d078a9c71d
CESAR ALESSANDRO MEJIA PEREZ <camejiap@correo.url.edu.gt>
2022-09-03 15:19:49 -0600

    Primer hola mundo y pause

---
+++ Estudio Independiente/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>//libreria de intup output stream
+
+using namespace std;//permite que no sea necesario escribir std antes de instrucciones de input, output
+
+int main() {
+
+	cout << "Hola Mundo"<<endl;
+
+	system("pause");
+
+	return 0;
+}
