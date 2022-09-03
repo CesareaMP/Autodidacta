47465672bbd38bc02e9bbc76fc37b2b96613e40b
CESAR ALESSANDRO MEJIA PEREZ <camejiap@correo.url.edu.gt>
2022-09-03 15:52:39 -0600

    TIPOS DE DATOS CON ENTRADAS Y SALIDAS

--- Estudio Independiente/Source.cpp
+++ Estudio Independiente/Source.cpp
@@ -2,11 +2,71 @@
 
 using namespace std;//permite que no sea necesario escribir std antes de instrucciones de input, output
 
-int main() {
+//TIPOS DE DATOS
+/*int main() { TIPOS DE DATOS
 
-	cout << "Hola Mundo"<<endl;
+	int entero = 15;//tipo de dato entero
+	float flotante = 10.45;//tipo de dato decimal de menor tamaño
+	double decimal = 16.3456;//tipo de dato decimal de mayor tamaño
+	char letra = 'a';//tipo de dato caracter
+
+	cout <<entero<<endl;
+	cout << flotante << endl;
+	cout << decimal <<endl;
 
 	system("pause");
 
 	return 0;
+}*/
+
+//ENTRADA Y SALIDA
+/*int main() {
+
+	int numero;
+
+	cout << "Digite un numero" << endl;
+	cin >> numero;
+
+	cout << "El numero digitado es:" << numero;
+
+	system("pause");
 }
+*/
+
+//PROBLEMA 1A: INGRESO DE DOS NUMEROS Y MOSTRAR SUMA, RESTA, MULTIPLICACION Y DIVISION
+/*int main() {
+
+	int n1, n2,suma=0,resta=0,multi=0,divi=0;
+
+	cout << "Ingrese un numero: ";
+	cin >> n1;
+	cout << "Ingrese otro numero:";
+	cin >> n2;
+
+	suma = n1 + n2;
+	resta = n1 - n2;
+	multi = n1 * n2;
+	divi = n1 / n2;
+
+	cout << "Resultado de suma: " << suma << "\nResultado de la resta: " << resta << "\nResultado de la multiplicacion: " << multi << "\nResultado de la division: " <<divi<<endl;
+
+	system("pause");
+}
+*/
+
+//PROBLEMA 2A: RECIBIR EL PRECIO DE UN PRODUCTO Y SE LE APLIQUE EL IVA
+/*int main() {
+
+	float producto, piva=0;
+
+	cout << "Ingrese el valor del producto: ";
+	cin >> producto;
+
+	piva = producto*1.12;
+
+	cout << "El precio del producto con iva es de: " << piva;
+
+	system("pause"); 
+
+	return 0;
+}*/
